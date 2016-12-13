bdf0b97e1900aa26a6ad795846dc2d798e21a20d
Reidv <reidvacek@hotmail.com>
2016-12-13 13:53:56 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -21,7 +21,7 @@
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
-node `reidv.puppetlabs.vm`
+node 'reidv.puppetlabs.vm'
 {
 notify { "This will only be enforced on the Linux container.": }
 }
