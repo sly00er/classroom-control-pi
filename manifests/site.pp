b84346f5314b4fcdd56f046ac68510ff814eb481
Laura Macchi <lau.macchi@gmail.com>
2016-12-13 13:40:00 -0600

    Added a node definition for lmacchi.puppetlabs.vm

--- manifests/site.pp
+++ manifests/site.pp
@@ -21,6 +21,10 @@
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
+node 'lmacchi.puppetlabs.vm' {
+  notify { "This is the Linux VM for Laura": }
+}
+
 node default {
   # This is where you can declare classes for all nodes.
   # Example:
