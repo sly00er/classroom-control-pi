8b270710c589a137a20d0ca145c9499fe04b8421
Laura Macchi <lau.macchi@gmail.com>
2016-12-14 11:01:04 -0600

    Add a warning

--- manifests/site.pp
+++ manifests/site.pp
@@ -21,14 +21,6 @@
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
-node /puppetlabs\.vm/ {
-  notify { "This is a regular expression": }
-}
-
-node 'lmacchi.puppetlabs.vm' {
-  notify { "This is the Linux VM for Laura": }
-}
-
 node default {
   # This is where you can declare classes for all nodes.
   # Example:
@@ -38,4 +30,7 @@ node default {
   include examples::puppetize
   
   notify { "This is the LMacchi branch": }
+  unless $environment in ['production','staging'] {
+    notify { "Warning: this is a development environment on ${::fqdn}": }
+  }
 }
