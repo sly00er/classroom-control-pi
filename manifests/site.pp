dca0cb0a0912ae6e5bf075cb843427253a98ea7c
smanekia <siddybob69@gmail.com>
2016-12-14 10:51:02 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -27,7 +27,9 @@ node default {
   #   class { 'my_class': }
   
   # example code for the classroom
-  include examples::puppetize
-  
-  notify { "Hello world! I am ${::fqdn}": }
+  node default {
+unless $environment in [ 'production', 'staging' ] {
+notify { "Warning: this is a development environment on ${::fqdn}": } }
+# ...
 }
+
